87bc6e5006c441507d0829e7de27289c834fa128
choidam <37874238+choidam@users.noreply.github.com>
2022-04-12 14:37:02 +0900

    spm dependencies 추가

--- KIDA/Tuist/Project.swift
+++ KIDA/Tuist/Project.swift
@@ -21,6 +21,17 @@ import ProjectDescriptionHelpers
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
 
+//let carthageDependencies: [CarthageDependencies.Dependency] = [
+//    .github(path: "https://github.com/ReactorKit/ReactorKit", requirement: .exact("2.1.1")),
+//    .github(path: "https://github.com/RxSwiftCommunity/RxDataSources", requirement: .exact("4.0.1")),
+//    .github(path: "https://github.com/ReactiveX/RxSwift", requirement: .exact("5.1.3")),
+//    .github(path: "https://github.com/SnapKit", requirement: .exact("5.0.1")),
+//    .github(path: "https://github.com/devxoul/Then", requirement: .exact("2.7.0"))
+//]
+//
+//let dependencies = Dependencies(carthage: .init(carthageDependencies), platforms: [.iOS])
+
+
 let project = Project(name: "KIDA",
                       targets: [
                         Target(
@@ -29,7 +40,20 @@ let project = Project(name: "KIDA",
                             product: .app,
                             bundleId: "com.ian.KIDA",
                             infoPlist: .file(path: "../KIDA/Info.plist"),
-                            sources: ["../KIDA/**"]
+                            sources: ["../KIDA/**"],
+                            dependencies: [
+                                .package(product: "ReactorKit"),
+                                .package(product: "RxDataSources"),
+                                .package(product: "RxSwift"),
+                                .package(product: "SnapKit"),
+                                .package(product: "Then")
+                            ]
                         )
                       ]
 )
+
+public extension TargetDependency {
+    static func carthage(name: String) -> TargetDependency {
+        .framework(path: Path("Tuist/Dependencies/Carthage/iOS/\(name).framework"))
+    }
+}
