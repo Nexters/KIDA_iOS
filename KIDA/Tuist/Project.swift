7b13806651512c19bb7812821d395e8f3e973e15
choidam <37874238+choidam@users.noreply.github.com>
2022-04-12 14:49:03 +0900

    주석 코드 제거

--- KIDA/Tuist/Project.swift
+++ KIDA/Tuist/Project.swift
@@ -19,19 +19,6 @@ import ProjectDescriptionHelpers
 
 // MARK: - Project
 
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
-
-//let carthageDependencies: [CarthageDependencies.Dependency] = [
-//    .github(path: "https://github.com/ReactorKit/ReactorKit", requirement: .exact("2.1.1")),
-//    .github(path: "https://github.com/RxSwiftCommunity/RxDataSources", requirement: .exact("4.0.1")),
-//    .github(path: "https://github.com/ReactiveX/RxSwift", requirement: .exact("5.1.3")),
-//    .github(path: "https://github.com/SnapKit", requirement: .exact("5.0.1")),
-//    .github(path: "https://github.com/devxoul/Then", requirement: .exact("2.7.0"))
-//]
-//
-//let dependencies = Dependencies(carthage: .init(carthageDependencies), platforms: [.iOS])
-
-
 let project = Project(name: "KIDA",
                       targets: [
                         Target(
